cd5fb33db9d5341611f9121c0959cbcf62d5b7b7
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-02-25 01:16:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 //your JS code here. If required.
-const myObj = {
-  name: "John",
-  age: 30,
-  city: "New York"
+const student = {
+    name: "John"
 };
 
-function getKeys(myObj) {
-  return Object.keys(myObj);
+function getKeys(obj) {
+    return Object.keys(obj);
 }
 
-console.log(getKeys(myObj));
+console.log(getKeys(student));
